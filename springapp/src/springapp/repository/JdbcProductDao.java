dd90dca579e23f10216090ed7d9be73ece2f1c0b
Jef Nackaerts <jef@Sophie.local>
2013-06-05 17:06:13 +0200

    Added war to gitignore and removed it from repository.

--- springapp/src/springapp/repository/JdbcProductDao.java
+++ springapp/src/springapp/repository/JdbcProductDao.java
@@ -8,12 +8,9 @@ import javax.sql.DataSource;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import springapp.domain.Product;
